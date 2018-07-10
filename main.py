a417923e436c068ce8dd128440b358572509bf3a
ptrk <ptrk@ntxt.net>
2018-07-10 21:32:12 +0200

    removed logging, enforced unique dest paths, cmdline params

--- main.py
+++ main.py
@@ -4,10 +4,6 @@ from time import sleep
 import sys
 import re
 
-def open_batch(batchCount, timestamp):
-    fname = "img-batch-{0}-{1}.txt".format(timestamp, batchCount)
-    return open(fname, "w")
-
 # returns a pair of paths - source and destination. The destination is based
 # on the name of the source path if it matches the format YYYY-MM-DD, otherwise on the
 # "client_modified" property, which is when it was uploaded to Dropbox first.
@@ -24,18 +20,41 @@ def create_new_path(entry, destFolder):
         year, month, day = entry.client_modified.timetuple()[:3]
     
     return [
-        entry.path_display, 
-        fnameDest.format(destFolder, year, month, day, fnameSrc)
+            entry.path_lower, 
+            fnameDest.format(destFolder, year, month, day, fnameSrc)
         ]
+
+def unique_items( batch ):
+    index = {}
+    unique = []
+    for item in batch:
+        path = item.to_path.lower()
+        newpath = item.to_path
+        if(path in index):
+            occurenceNo = index[path]
+            newpath = re.sub(
+                r"\.(.*)$", 
+                "-%s.\1" % occurenceNo, 
+                item.to_path
+            )
+            index[path] += 1
+        else:
+            index[path] = 1
+        unique.append(dropbox.files.RelocationPath(item.from_path, newpath))
+    return unique
+
+
 # --------------------------------------------------
 # Take the API token from the cmd arguments and start
-if( len(sys.argv) < 2 ):
-    print("missing argument - Dropbox API token")
+if( len(sys.argv) != 4 ):
+    print("USAGE: " + sys.argv[0] + " API_TOKEN '/source/folder' 'destination/folder'")
     sys.exit(1)
 token = sys.argv[1]
-srcFolder = '/Camera Uploads'
-destFolder = '/Photos'
-maxItemsPerBatch = 200
+srcFolder = sys.argv[2]
+destFolder = sys.argv[3]
+
+print("About to move files from folder {0} to {1}.".format(srcFolder, destFolder))
+maxItemsPerBatch = 100
 timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 dbx = dropbox.Dropbox(token)
 print (dbx.users_get_current_account())
@@ -45,7 +64,6 @@ print
 
 imgList = dbx.files_list_folder(srcFolder, True)
 batchCount = 0
-batchFile = False
 batches = []
 index = 0
 
@@ -61,30 +79,32 @@ while True:
 
         # open a new batch for every 'maxItemsPerBatch' items
         if(index % maxItemsPerBatch == 0):
-            try:
-                batchFile.close
-            except AttributeError:
-                pass
-            batchFile = open_batch(batchCount, timestamp)
             batches.append( [] )
             batchCount += 1  
+
         fnameSrc, fnameDest = create_new_path(entry, destFolder)
         relocation = dropbox.files.RelocationPath(fnameSrc, fnameDest)
         batches[batchCount-1].append( relocation )
         logLine = fnameSrc + " -> " + fnameDest + "\n"
-        batchFile.write( logLine )
+        #batchFile.write( logLine )
         index += 1
+    
     if( not imgList.has_more ):
         break
-    print ("fetching next chunk")
+    print ("{0} files found, fetching next chunk".format(index))
     imgList = dbx.files_list_folder_continue( imgList.cursor )
-batchFile.close
+
+if(len(batches) == 0):
+    print ("Nothing to do apparently")
+    exit(0)
 
 # batches ready to be moved, now we need to do it sequentially to avoid
 # "too many write operations" error from Dropbox    
 
+print("Sending batches to move, {0} files each".format(maxItemsPerBatch))
 results = []
 for i, batch in enumerate(batches):
+    batch = unique_items(batch)
     job = dbx.files_move_batch( batch, True, True, True )
     id = job.get_async_job_id()
     if (not job.is_complete()):
@@ -92,13 +112,17 @@ for i, batch in enumerate(batches):
             print ("checking job ", id)
             try:
                 result = dbx.files_move_batch_check(id)
-            except dropbox.exceptions.ApiError:
+            except dropbox.exceptions.ApiError as err:
+                print("API error: ", err)
+                for item in batch: print(item)
                 results.append( "failed" )
                 break               
             if( result.is_complete() ):
                 results.append( "complete" )
                 break
             elif( result.is_failed() ):
+                print(result,"\n\n")
+                for item in batch: print(item)
                 results.append( "failed" )
                 break
             else:
